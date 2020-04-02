7afa04dee03c1573d0bb3ba839d42ea92b940e8b
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2020-04-02 19:25:22 +0200

    get the function name from its code

--- src/app/models/function.rb
+++ src/app/models/function.rb
@@ -6,11 +6,32 @@ class Function < ApplicationRecord
 
   belongs_to :user
 
+  before_validation :function_name
+
   def to_param
     name
   end
 
   def self.from_param(param)
     find_by(name: param)
+  end
+
+  private
+
+  def find_first_function(ast)
+    return ast.children.first.to_s if ast.respond_to?('type') && ast.type == :DEFN
+
+    return unless ast.respond_to?('children')
+
+    ast.children.each do |child|
+      func_name = find_first_function(child)
+      return func_name if func_name
+    end
+  end
+
+  def function_name
+    self.name = find_first_function(RubyVM::AbstractSyntaxTree.parse(code))
+  rescue ErrorClass
+    self.name = nil
   end
 end
